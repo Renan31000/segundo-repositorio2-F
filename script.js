76ef1bb8b135c597cc33b1f745a133853f03d7d1
Renan31000 <renan.menezes.lima@escola.pr.gov.br>
2025-06-26 21:27:58 -0300

    aula 3

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
+botaoMostraPalavras.addEventListener('click', mostrarPalavraChave);
 
-function mostraPalavraChave() {
-    alert("Fui Clicado");
+function mostrarPalavraChave() {
+    //alert("Fui Clicado!");
+    const texto = Document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+
+    campoResultado.textContent = texto;
 }
