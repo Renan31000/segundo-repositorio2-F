ea020dee08d66ae385d99a4a7e141ecfa7f8804a
Renan31000 <renan.menezes.lima@escola.pr.gov.br>
2025-06-26 21:34:15 -0300

    aula 1 certo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ botaoMostraPalavras.addEventListener('click', mostrarPalavraChave);
 
 function mostrarPalavraChave() {
     //alert("Fui Clicado!");
-    const texto = Document.querySelector('#entrada-de-texto').value;
+    const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
 
     campoResultado.textContent = texto;
